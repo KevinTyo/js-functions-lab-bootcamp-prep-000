28cd9d203fdf93b94dac703def422fbd535d9a56
kevin.d.tyo@gmail.com <kevin.d.tyo@gmail.com>
2018-08-10 22:45:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  name = "you"
+  return 'Happy holidays, ${name}!'
+}
+function happyHolidayto(holiday, name) {
+  holiday = "Mother's Day"
+  name = "Mom"
+  return 'Happy ${Christmas}, ${name}'
+}
+function holidayCountdown(holiday, days) {
+  holiday = "Mother's Day"
+  days = 20
+  return 'It\'s ${days} days until ${Christmas}'
+}
